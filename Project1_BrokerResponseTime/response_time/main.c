d8576bbe4d84e5e24254eaa29cf5345470bf7c4e
Uros <Cvjetinovic>
2024-06-24 22:41:38 +0200

    Update after testing time response

--- Project1_BrokerResponseTime/response_time/main.c
+++ Project1_BrokerResponseTime/response_time/main.c
@@ -2,30 +2,36 @@
 * Authors:
 *    Uros Popovic & Uros Cvjetinovic
 * @file
-* \brief This program is used to test response time
+* \brief This program is used to test response time of broker
 *******************************************************************************/
 
 /******* include headers ******************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <windows.h>
 #include "MQTTClient.h"
 
 /******* local macros *********************************************************/
-#define ADDRESS       "tcp://broker.hivemq.com:1883"
-#define CLIENTID      "ResponseCheck"
-#define TOPIC         "ResponseTest"
-#define QOS           2
-#define TIMEOUT       10000L
+#define ADDRESS                "tcp://broker.hivemq.com:1883"
+#define CLIENTID               "ResponseCheck"
+#define TOPIC                  "ResponseTest"
+#define QOS                    2
+#define TIMEOUT                10000L
+#define NUMBEROFMSGTOSEND      ((uint32_t) 100u)
+#define DEBUGLOG               0
 
 /******* local data objects ***************************************************/
+MQTTClient client;
 volatile MQTTClient_deliveryToken deliveredtoken;
 LARGE_INTEGER startTime;
 LARGE_INTEGER endTime;
 LARGE_INTEGER frequency;
 double totalTime = 0;
-int messageCount = 0;
+uint32_t connectionLost = 0;
+uint32_t messagePublished = 0;
+uint32_t messageArrived = 0;
 
 /******* declaration of local functions ***************************************/
 void delivered(void *context, MQTTClient_deliveryToken deliveryToken);
@@ -40,90 +46,150 @@ int main(int argc, char* argv[]);
 /******* definition of local functions ****************************************/
 void tokenDeliveredHandler(void *context, MQTTClient_deliveryToken deliveryToken) 
 {
-  printf("Message with token value %d delivery confirmed\n", deliveryToken);  // TODO: remove
+  QueryPerformanceCounter(&endTime);
+
+  double elapsedTime = (double)(endTime.QuadPart - startTime.QuadPart) *
+                        1000.0 / frequency.QuadPart;
+  totalTime += elapsedTime;
+  messageArrived++;
+#if (DEBUGLOG)
+  printf("Message with token value %d delivery confirmed\n", deliveryToken);
+  printf("-Response time: %.2f ms\n", elapsedTime);
+#endif
   deliveredtoken = deliveryToken;
 }
 
 int messageArrivedHandler(void *context, char *topicName, int topicLen,
                           MQTTClient_message *message) 
 {
-  QueryPerformanceCounter(&endTime);
-
-  double elapsedTime = (double)(endTime.QuadPart - startTime.QuadPart) *
-                        1000.0 / frequency.QuadPart;
-  totalTime += elapsedTime;
-  messageCount++;
-  printf("Recived:\n");  // TODO: remove
-  printf("-Topic: %s\n", topicName);  // TODO: remove
-  printf("-Message: %.*s\n", message->payloadlen, (char*)message->payload);  // TODO: remove
-  printf("-Response time: %.2f ms\n", elapsedTime);  // TODO: remove
-
-  MQTTClient_freeMessage(&message);
-  MQTTClient_free(topicName);
+#if (DEBUGLOG)
+  printf("Recived message.\n");
+#endif
   return 1;
 }
 
 void connectionLostHandler(void *context, char *cause) 
 {
+  connectionLost = 1u;
+#if (DEBUGLOG)
   printf("\nConnection lost\n");
   printf("-Cause: %s\n", cause);
+#endif
 }
 
 int main(int argc, char* argv[]) 
 {
   int result = 0;
+  MQTTClient_connectOptions connectionOptions = MQTTClient_connectOptions_initializer;
+  MQTTClient_message publishMessage = MQTTClient_message_initializer;
+  MQTTClient_deliveryToken token;
+  int numberOfConnectRetries = 100u;
+  char payload[20];
+  char userInput;
+  
+  MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE,
+                    NULL);
+  connectionOptions.keepAliveInterval = 20;
+  connectionOptions.cleansession = 1;
+  if (QOS == 2)
+  {
+    // Needed for QOS 2, add also in hive websocket client
+    connectionOptions.username = "Uros";
+    connectionOptions.password = "1";
+  }
+  
+  publishMessage.qos = QOS;
+  publishMessage.retained = 0;
+
+  MQTTClient_setCallbacks(client, NULL, connectionLostHandler, 
+                          messageArrivedHandler, tokenDeliveredHandler);
+  
+  result = MQTTClient_connect(client, &connectionOptions);
+  if (result != MQTTCLIENT_SUCCESS) 
+  {
+    printf("Failed to connect, return code %d\n", result);
+    exit(EXIT_FAILURE);
+  }
   
   while(1) 
   {
-    MQTTClient client;
-    MQTTClient_connectOptions connectionOptions = MQTTClient_connectOptions_initializer;
-    MQTTClient_message publishMessage = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
-
-    MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE,
-                      NULL);
-    connectionOptions.keepAliveInterval = 20;
-    connectionOptions.cleansession = 1;
-
-    MQTTClient_setCallbacks(client, NULL, connectionLostHandler, 
-                            messageArrivedHandler, tokenDeliveredHandler);
-    
-    result = MQTTClient_connect(client, &connectionOptions);
-    if (result != MQTTCLIENT_SUCCESS) 
-    {
-      printf("Failed to connect, return code %d\n", result);
-      exit(EXIT_FAILURE);
-    }
-    
-    MQTTClient_subscribe(client, TOPIC, QOS);
     QueryPerformanceFrequency(&frequency);
 
-    for (int i = 0; i <= 100; i++) 
+    for (int i = 0; i <= NUMBEROFMSGTOSEND; i++) 
     {
-      char payload[20];
-      snprintf(payload, sizeof(payload), "%d", i);
+      snprintf(payload, sizeof(payload), "%d", messagePublished++);
       publishMessage.payload = payload;
-      publishMessage.payloadlen = strlen(payload);
-      publishMessage.qos = QOS;
-      publishMessage.retained = 0;
+      publishMessage.payloadlen = (int) strlen(payload);
 
+#if (DEBUGLOG)
+      printf("Publication of: of %s\n", payload);
+#endif
       QueryPerformanceCounter(&startTime);
       MQTTClient_publishMessage(client, TOPIC, &publishMessage, &token);
-      printf("Waiting for publication of %s\n", payload);  // TODO: remove
       result = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-      printf("Message with delivery token %d delivered\n", token);  // TODO: remove
-      Sleep(10);
+
+      if(result = MQTTCLIENT_SUCCESS)
+      {
+#if (DEBUGLOG)
+        printf("Message with delivery token %d delivered\n", token);
+#endif
+      }
+      else if (QOS == 0)
+      {
+        // Since QOS0 doesn't provide message arrived, we can only check if delivered
+        QueryPerformanceCounter(&endTime);
+      
+        double elapsedTime = (double)(endTime.QuadPart - startTime.QuadPart) *
+                              1000.0 / frequency.QuadPart;
+        totalTime += elapsedTime;
+        messageArrived++;
+#if (DEBUGLOG)
+        printf("Message arrived based on completion for QOS0.\n");
+        printf("-Response time: %.2f ms\n", elapsedTime);
+#endif
+      }
+      
+      if ((messagePublished >= 256u) && (i != NUMBEROFMSGTOSEND))
+      {
+        messagePublished = 0u;
+      }
+      
+      if (connectionLost == 1u)
+      {
+        result = MQTTCLIENT_FAILURE;
+        while ((result != MQTTCLIENT_SUCCESS) && (numberOfConnectRetries > 0))
+        {
+          result = MQTTClient_connect(client, &connectionOptions);
+          numberOfConnectRetries--;
+        }
+        
+        if (result != MQTTCLIENT_SUCCESS) 
+        {
+          printf("Failed to connect, return code %d\n", result);
+          exit(EXIT_FAILURE);
+        }
+        else
+        {
+          connectionLost = 0u;
+        }
+      }
     }
-
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-
-    if (messageCount > 0) 
+  
+    if (messageArrived > 0) 
     {
-      printf("Average response time: %.2f ms\n", totalTime / messageCount);
-      Sleep(100);
+      printf("For QOS %d :\n", QOS);
+      printf("Average response time: %.2f ms\n", totalTime / messageArrived);
+      printf("Press Q key to quit, or any other to continue.\n");
+      scanf("%c", &userInput);
+      if(userInput == 81u || userInput == 113u) 
+      {
+        break;
+      }
     }
   }
-  
+
+  MQTTClient_disconnect(client, 10000);
+  MQTTClient_destroy(&client);
+
   return result;
 }
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # Mqtt_simple_project
+
+Co-Author: Uros Popovic, github: https://github.com/urospopovic1996
+
+Additional resources: time control. The task consists of two parts.
+To control timing within a C program, one of the standard libraries contained in time.h is used. The library should be included with #include <time.h> in the program.
+
+## Part 1 - Response time of broker
+Write a program that tests the average response time of a broker:
+ - Implement a client program that subscribes to a topic and continuously publishes test messages of arbitrary but varying content to the same topic, for example, numbers from 0 to 255.
+ - Immediately before sending the first message to the topic, start a timer.
+ - As soon as the client receives the 100th message back from the broker, stop the timer and record the time interval.
+ - Divide the time interval by 100 to obtain the average response time.
+ - Repeat steps 3 and 4 in an infinite loop.
+ - Print the average response time on the screen, one below the other, after each measurement cycle.
+ - Implement any command to exit the program.
+ - Evaluate the average response time and deviation from the average time for all QOS types.
+ 
+## Part 2 - Simulate a industry process
+Choose QoS for minimum fluctuation in average response time. The average response time is labeled Tp
+The system simulating pressure control in an industrial process is depicted in the following image:
+
+![Slika](link_to_image)
+
+a) The industrial process is described by a differential equation modeling a first-order system with a time constant of 10s and gain of 1:
+\[ y[n] = \frac{1}{21} \left( x[n] + x[n-1] \right) + \frac{19}{21} y[n-1] \]
+Here, \( y[n] \) represents the pressure \( p(t) \) in relative units [0-100%]. Sampling of \( y[n] \) based on the input signal occurs with a period \( T_s = 1 \)s (using a timer). All initial values for the differential equation are 0. Whenever a new value of \( p(t) \) is generated, it is written to the corresponding topic. A mobile application subscribes to this topic and graphs the pressure values every second whenever a new \( y[n] \) is received. Additionally, the mobile application generates the desired pressure \( p_z(t) \) via a slider ranging from 10 to 90, which is written to another corresponding topic.
+
+b) The controller subscribes to topics for current pressure \( p(t) \) (received every second) and desired pressure \( p_z(t) \) (received when the slider is adjusted). The controller implements hysteresis ON-OFF control such that if the current pressure \( p(t) \) exceeds \( p_z(t) \) by deltaP, it writes 0 to the appropriate topic. If \( p(t) \) is less than \( p_z(t) \) by deltaP, it writes 100 to the topic. If within bounds, nothing is written. The industrial processes subscribe to this topic, receiving either 0 or 100 from the broker, which is then stored in \( x_{temp} \). At each sampling instance \( T_s \), \( x[n] \) is set to \( x_{temp} \).
+
+c) Run the simulation for deltaP = 5. Document the regulation quality for several slider positions: 10, 40, 60, 80. Repeat for deltaP = 1.
+
+## Project requirements:
+**Integrated Computer Systems 2, project 7b**
+
+Write a program that enables synchronous reading of a 3D accelerometer and 3D gyroscope using the X-NUCLEO-IKS01A2 sensor board. The acquired data needs to be transmitted via a serial connection to a computer and graphically displayed on an OLED display.
+
+# Report 
+## How to set up
+### Software
+ - Clone repo:
+```
+   git clone --recurse-submodules https://github.com/UrosCvjetinovic/Mqtt_simple_project.git
+```
+ - Environment:
+```
+   Embarcadero_Dev-Cpp_6.3_TDM-GCC_9.2_Setup
+```
+ - Set up broker with configured topic (see source code):
+```
+   https://www.hivemq.com/demos/websocket-client/
+```
+ 
+## Implementation 
+
+### Part 1 - Response time of broker
+
+#### Logic
+
+#### Testing
+
+##### QOS 0
+Average response time: 0.06-0.07 ms
+
+![QOS0](https://github.com/UrosCvjetinovic/Project1_BrokerResponseTime/images/QOS0.png)
+ 
+
+##### QOS 1
+Average response time: 67-68 ms
+
+![QOS1](https://github.com/UrosCvjetinovic/Project1_BrokerResponseTime/images/QOS1.png)
+ 
+
+##### QOS 2
+Average response time: 149-151 ms
+
+![QOS2](https://github.com/UrosCvjetinovic/Project1_BrokerResponseTime/images/QOS2.png)
+ 
+ 
+### Part 2 - Simulate a industry process
+
+#### Logic
+
+#### Testing
